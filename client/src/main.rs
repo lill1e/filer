0759ae8aed62c3105627ee6523ab3b679bc42fda
lillie <onlinechronically@protonmail.com>
2024-12-31 14:11:42 -0500

    add: functional client code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
+    let path = "example.mp4";
     println!("Hello, world!");
+    match ureq::post("https://clips.lillie.rs/upload").send_multipart_file("file", path) {
+        Ok(res) => match res.status() {
+            200 => match res.into_json::<UploadResult>() {
+                Ok(value) => println!("Placeholder message: {:?}", value),
+                Err(error) => println!("An error occured with the upload request: {}", error),
+            },
+            _ => match res.into_json::<UploadError>() {
+                Ok(value) => println!(
+                    "An error occured with the upload request: {}",
+                    value.message
+                ),
+                Err(error) => println!("An error occured with the upload request: {}", error),
+            },
+        },
+        Err(error) => println!("An error occured with the upload request: {}", error),
+    };
 }
