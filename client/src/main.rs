00677da6228f5b8e7fde6c2bfe145c23964412a6
lillie <onlinechronically@protonmail.com>
2024-12-31 14:16:12 -0500

    delegate: file path to program arguments

--- client/src/main.rs
+++ client/src/main.rs
@@ -13,9 +13,13 @@ struct UploadError {
 }
 
 fn main() {
-    let path = "example.mp4";
-    println!("Hello, world!");
-    match ureq::post("https://clips.lillie.rs/upload").send_multipart_file("file", path) {
+    if let None = env::args().skip(1).next() {
+        println!("Please provide a file to upload");
+        return;
+    }
+    match ureq::post("https://clips.lillie.rs/upload")
+        .send_multipart_file("file", env::args().skip(1).next().unwrap())
+    {
         Ok(res) => match res.status() {
             200 => match res.into_json::<UploadResult>() {
                 Ok(value) => println!("Placeholder message: {:?}", value),
