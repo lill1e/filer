8eebaa81f87c5aaf536071dd31d161630ea9fb48
lillie <onlinechronically@protonmail.com>
2025-01-18 18:25:45 -0500

    add: sql setup script

---
+++ db.sql
@@ -0,0 +1,23 @@
+USE filer;
+
+CREATE TABLE users (
+    id PRIMARY VARCHAR(20) NOT NULL,
+    username VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE uploads (
+    id PRIMARY VARCHAR(20) NOT NULL,
+    file TEXT NOT NULL,
+    owner VARCHAR(20) NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    finished BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE alerts (
+    owner VARCHAR(20) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    message TEXT,
+    upload INTEGER,
+    upload_name TEXT
+);
